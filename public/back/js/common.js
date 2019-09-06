cc3cfc7dd7a9bc2e275ee2306bbd8e8ffaafcb33
wally210 <812540137@qq.com>
2019-09-06 12:52:50 +0800

    index页面公共部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -26,3 +26,64 @@ $(document).ajaxStop(function(){
         NProgress.done();
     }, 500);
 });
+
+// 登录拦截功能, 登录页面不需要校验, 不用登录就能访问
+// 前后分离了, 前端是不知道该用户是否登录了, 但是后台知道,
+// 发送 ajax请求, 查询用户状态即可
+// (1) 用户已登录, 啥都不用做, 让用户继续访问
+// (2) 用户未登录, 拦截到登录页
+if(location.href.indexOf("login.html") === -1){
+    $.ajax({
+        url: "/employee/checkRootLogin",
+        type: "get",
+        dataType: "json",
+        success: function(info){
+            console.log(info);
+            if(info.success){
+                // 已登陆，继续访问
+            }
+            if(info.error === 400){
+                // 未登陆，跳转到登陆页login.html
+                location.href = "login.html";
+            }
+        }
+    })
+}
+
+
+$(function(){
+    // 1.分类管理切换功能
+    $(".nav .category").on("click", function(){
+        // 切换child的显示隐藏
+        $(".nav .child").stop().slideToggle();
+    });
+
+    // 2.左侧边栏切换功能
+    $(".icon-menu").on("click", function(){
+        $(".lt-aside").toggleClass("hidemenu");
+        $(".lt-main").toggleClass("hidemenu");
+    });
+
+    // 3.点击topbar退出按钮，弹出模态框
+    $(".icon-logout").on("click", function(){
+        // 显示模态框, 显示模态框 modal("show");
+        $("#logout-modal").modal("show");
+    });
+
+    // 4.点击模态框退出按钮，实现退出功能
+    $("#btn-logout").on("click",function(){
+        $.ajax({
+            type: "get",
+            url: "/employee/employeeLogout",
+            dataType: "json",
+            success: function(info){
+                console.log(info);//{success: true}
+                if(info.success){
+                    // 退出成功，跳转到登陆页面
+                    location.href = "login.html";
+                }
+            }
+        })
+    })
+})
+
