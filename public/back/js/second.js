efbaa7d52b811720a852c859110b9e5dbd2734b6
wally210 <812540137@qq.com>
2019-09-08 13:23:11 +0800

    二级分类分页完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -2,9 +2,9 @@ $(function(){
     var currentPage = 1;
     var pageSize = 5;
     
-    sender();
+    render();
     // 1渲染二级分类页面
-    function sender(){
+    function render(){
         $.ajax({
             url: "/category/querySecondCategoryPaging",
             type: "get",
@@ -17,6 +17,21 @@ $(function(){
                 console.log(info);
                 var htmlStr = template("secondTpl", info);
                 $(".lt-content tbody").html(htmlStr);
+
+                // 分页插件bootstrapPaginator(需要ul标签)
+                $("#paginator").bootstrapPaginator({
+                    // 版本号
+                    bootstrapMajorVersion: 3,
+                    // 当前页
+                    currentPage: info.page,
+                    // 总页数
+                    totalPages: Math.ceil(info.total/info.size),
+                    // 页码点击事件
+                    onPageClicked: function(a, b, c, page){
+                        currentPage = page,
+                        render();
+                    }
+                })
             }
         })
     }
