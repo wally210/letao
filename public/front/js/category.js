3c160f3c619c9062358c0db8a713d71d28fc27da
wally210 <812540137@qq.com>
2019-09-12 10:49:32 +0800

    分类页面完成

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -3,6 +3,44 @@ $(function(){
     // 一进页面加载一级分类
     render()
     function render(){
-        
+        $.ajax({
+            type: "get",
+            url: "/category/queryTopCategory",
+            dataType: "json",
+            success: function(info){
+                console.log(info);
+                var htmlStr = template("categoryTpl", info);
+                $(".lt-category-left ul").html(htmlStr);
+                // 一进页面渲染二级分类
+                renderSecondById(info.rows[0].id);
+            }
+        });
+    }
+
+    // 点击切换一级分类，渲染相应的二级分类(委托事件)
+    $(".lt-category-left").on("click", "a", function(){
+        // 选中的加上.current，其它的去掉current
+        $(this).addClass("current").parent().siblings().find("a").removeClass("current");
+        // 渲染相应的二级分类
+        var id = $(this).data("id");
+        renderSecondById(id);        
+    });
+
+
+    // 根据一级分类id，渲染二级分类
+    function renderSecondById( id ){
+        $.ajax({
+            type: "get",
+            url: "/category/querySecondCategory",
+            data: {
+                id: id
+            },
+            dataType: "json",
+            success: function(info){
+                console.log(info);
+                var htmlStr = template("secondTpl", info);
+                $(".lt-category-right ul").html(htmlStr);
+            }
+        });
     }
 })
--- public/front/js/common.js
+++ public/front/js/common.js
@@ -1,5 +1,6 @@
 mui('.mui-scroll-wrapper').scroll({
     // scrollY: false, // 是否竖向滚动 是：true
     // bounce: true, // 是否弹回
+    indicators: true, //是否显示滚动条
 	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 });
